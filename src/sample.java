d3f4ede6255da502c909c13ed3e39d2905daae8d
chirag bhansali <cbhansal@asu.edu>
2019-02-06 23:08:57 -0700

    Just a sample file 2

--- src/sample.java
+++ src/sample.java
@@ -1,7 +1,25 @@
-public class sample{
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Sample {
 public static void main(String[] args)
 {
-int X = 12;
-System.out.println("Just testing");
+	Scanner saa = new Scanner(System.in);
+	int y = saa.nextInt();
+	String qq = "goasdk";
+	ArrayList<String> s = new ArrayList<String>();
+	s.add("sddsd");
+	s.add("dd");
+	System.out.println("Hello " + y + "sdsd"+ qq.length());
+	for (int i=0;i<6;i++)
+	{
+		y = Call(i);
+		System.out.println(y);
+	}
 }
+
+public static int Call(int i)
+{
+	return (i+23);
+}
+
 }
