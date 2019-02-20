16e91efd8e055dd543d3ba5fc4156c474da599cb
Ramanuj <psm-asu2@ghsc-psm.org>
2019-02-19 22:38:31 -0700

    File Structure Updated

--- Banking_System_Project/src/main/BankingSystemProjectApplication.java
+++ Banking_System_Project/src/main/BankingSystemProjectApplication.java
@@ -1,13 +1,14 @@
-package main.java;
+package main;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 import main.java.business.services.IUserServices;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"main.java.web","main.java.business.services","main.java.business.repositories"})
 public class BankingSystemProjectApplication implements CommandLineRunner{
 
 	@Autowired
---
+++ Banking_System_Project/src/main/java/web/Controllers.java
@@ -0,0 +1,18 @@
+package main.java.web;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controllers {
+
+
+    @RequestMapping(value="/opener", method = RequestMethod.GET)
+    public String opener(ModelMap model){
+        String name = (String) model.get("name");
+        model.put("open", name);
+        return "open";
+    }
+}
